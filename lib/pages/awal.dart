b5f4bcf760b85d6daa585d21ee60a72ea0aeee4b
daffashafwan <daffashafwan321@gmail.com>
2020-08-18 10:24:06 +0700

    add speech to text

---
+++ lib/pages/awal.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Awal extends StatefulWidget {
+  _AwalState createState() => _AwalState();
+}
+
+class _AwalState extends State<Awal> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+       child: Text('Learn'),
+    );
+  }
+}
